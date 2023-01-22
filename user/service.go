ef35aecb2e8a195ecd06b3067258a2e086bda49f
Azis Dzelani <azisdzelani@mhs.uinjkt.ac.id>
2023-01-22 16:23:10 +0700

    init project. Register user endpoint

---
+++ user/service.go
@@ -0,0 +1,37 @@
+package user
+
+import "golang.org/x/crypto/bcrypt"
+
+type Service interface {
+	RegisterUser(register RegisterUserInput) (User, error)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repository Repository) *service {
+	return &service{repository}
+}
+
+func (s *service) RegisterUser(input RegisterUserInput) (User, error) {
+	//mapping struct input ke struct User
+	//simpan struct User melalui repository
+	user := User{}
+	user.Name = input.Name
+	user.Occupation = input.Occupation
+	user.Email = input.Email
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.MinCost)
+	if err != nil {
+		return user, err
+	}
+	user.PasswordHash = string(passwordHash)
+	user.Role = "user"
+
+	newUser, err := s.repository.Save(user)
+	if err != nil {
+		return user, err
+	}
+
+	return newUser, nil
+}
